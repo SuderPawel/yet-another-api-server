f09fc1a82b5448679bad4254ee311fa1ae7687a7
Paweł Suder <pawel@suder.info>
2019-01-26 10:52:48 +0100

    Update migrations

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module YetAnotherApiServer
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
 
     # Assets
     config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,36 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227112433) do
+ActiveRecord::Schema.define(version: 2016_02_27_112433) do
 
   create_table "apis", force: :cascade do |t|
-    t.string   "name"
-    t.string   "uuid"
+    t.string "name"
+    t.string "uuid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["uuid"], name: "index_apis_on_uuid", unique: true
   end
-
-  add_index "apis", ["uuid"], name: "index_apis_on_uuid", unique: true
 
   create_table "events", force: :cascade do |t|
-    t.integer  "api_id"
-    t.string   "method"
+    t.integer "api_id"
+    t.string "method"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "body"
+    t.string "body"
+    t.index ["api_id"], name: "index_events_on_api_id"
   end
-
-  add_index "events", ["api_id"], name: "index_events_on_api_id"
 
   create_table "properties", force: :cascade do |t|
-    t.string   "name"
-    t.string   "source"
-    t.string   "value"
-    t.integer  "event_id"
+    t.string "name"
+    t.string "source"
+    t.string "value"
+    t.integer "event_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_properties_on_event_id"
   end
-
-  add_index "properties", ["event_id"], name: "index_properties_on_event_id"
 
 end
